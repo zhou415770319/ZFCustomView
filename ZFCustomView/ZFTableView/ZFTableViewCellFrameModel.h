9c5589e62eff23742d96797b661d1a27bc4712fd
zhoufei <415770319@qq.com>
2017-03-17 13:34:44 +0800

    fix to import

--- ZFCustomView/ZFTableView/ZFTableViewCellFrameModel.h
+++ ZFCustomView/ZFTableView/ZFTableViewCellFrameModel.h
@@ -7,8 +7,9 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import "ZFTableViewCellModel.h"
 @interface ZFTableViewCellFrameModel : NSObject
+
 
 @property(nonatomic,strong)ZFTableViewCellModel *cellInfo;
 
